1225b5e8609b4ae0999b9d91aee9d18e5a531a29
Ekrem Çetinkaya <ekrem.cetinkaya@ozu.edu.tr>
2022-08-22 11:37:31 +0200

    audio to mp3

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-import shutil
-
+import subprocess
 from pytube import YouTube
 
 
@@ -9,10 +8,17 @@ def main(link, audio_only=False):
     path = "./download/"
     yt = YouTube(link)
     if audio_only:
-        audio = yt.streams.filter(only_audio=True)[0].download(path)
+        audio = yt.streams.filter(only_audio=True)[0]
+        audio.download(path)
+        file_name = audio.default_filename
+        mp3_name = file_name[:-4] + ".mp3"
+        # convert to mp3
+        subprocess.run(["ffmpeg", "-i", os.path.join(path, file_name), os.path.join(path, mp3_name)])
+        # remove the video file
+        os.remove(os.path.join(path, file_name))
     else:
-        video = yt.streams.get_highest_resolution().download(path)
+        yt.streams.get_highest_resolution().download(path)
 
 
 if __name__ == '__main__':
-    main("https://www.youtube.com/watch?v=fdGyoJXeWrI",  audio_only=True)
+    main("https://www.youtube.com/watch?v=QfRU-1T8q4g&",  audio_only=True)
